64dce9ccf78c9caaf8c72894a297c75601a2e330
akaur20 <42978513+akaur20@users.noreply.github.com>
2018-09-16 19:13:37 -0400

    Add files via upload

---
+++ Variable Homework/src/VariableHomework.java
@@ -0,0 +1,24 @@
+
+public class VariableHomework 
+{
+
+	public static void main(String[] args) 
+	{
+		String name = "Avleen";
+		System.out.println(name);
+
+		int rectLength = 5;
+		int rectWidth = 8;
+		int rectArea = rectLength * rectWidth;
+		System.out.println("Area of Rectangle:" + rectArea);
+		
+		int radius = 5;
+		double pi = 3.14;
+		double circumference = 2 * radius * pi;
+		double area = pi * radius * radius;
+		System.out.println("Circumference:" + circumference);
+		System.out.println("Area of Circle:" + area);
+	
+	}
+			
+}
